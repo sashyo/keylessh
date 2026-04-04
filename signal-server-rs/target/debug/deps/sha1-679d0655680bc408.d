822d20d97370c722a312b93b6cf578665b8a358c
Sasha Le <iamsasha.le@gmail.com>
2026-04-04 20:58:40 +1000

    fix: remove target dir from git, fix gitignore

--- signal-server-rs/target/debug/deps/sha1-679d0655680bc408.d
+++
@@ -1,8 +0,0 @@
-/home/sasha/keylessh/signal-server-rs/target/debug/deps/sha1-679d0655680bc408.d: /home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/lib.rs /home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress.rs /home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/soft.rs /home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/x86.rs
-
-/home/sasha/keylessh/signal-server-rs/target/debug/deps/libsha1-679d0655680bc408.rmeta: /home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/lib.rs /home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress.rs /home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/soft.rs /home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/x86.rs
-
-/home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/lib.rs:
-/home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress.rs:
-/home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/soft.rs:
-/home/sasha/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/x86.rs:
